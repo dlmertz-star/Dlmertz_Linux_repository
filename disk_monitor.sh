f9123b1f7b4692bfb01782db6c321c7ef4673847
Desirae Mertz <dlmertz@andrew.cmu.edu>
2026-09-17 15:50:23 -0400

    finished code in disk_monitor

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -1,2 +1,16 @@
 #!/user/bin/env bash
 
+Threshold=90
+alert="/home/desirae-mertz/Dlmertz_Linux_repository/disk_alert.sh"
+
+df -h | grep -E '/dev/sd?|/dev/nvme?' | while read line
+do
+	filesystem=$(echo $line | awk '{print $1}')
+	percent=$(echo $line | awk '{print $5}')
+	percentNumber=$(echo $percent | tr -d '%')
+	if [ $percentNumber -ge $THRESHOLD]
+	then
+		$alert $filesystem $percent
+	fi
+done
+
