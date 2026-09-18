1520c2a256b8b64759bd8c67312a0cd04b271e9d
Desirae Mertz <dlmertz@andrew.cmu.edu>
2026-09-18 14:19:35 -0400

    Updated disk monitor to also be portable

--- disk_alert.sh
+++ disk_alert.sh
@@ -5,8 +5,8 @@ SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
 
 source "${SCRIPT_DIR}/disk_monitor.conf"
 
-DA_LOGFILE="~/disk-monitor.log"
-DA_FILESYSTEM=$1
-DA_PERCENT=$2
-DA_NOW=$(date --iso-8601=seconds)
-echo "${now} ${filesystem} ${percent}" >> $Logfile
+DA_LOGFILE="${DM_LOGFILE}"
+DA_FILESYSTEM="${1}"
+DA_PERCENT="${2}"
+DA_NOW="$(date --iso-8601=seconds)"
+echo "${DA_NOW} ${DA_FILESYSTEM} ${DA_PERCENT}%" >> "${DA_LOGFILE}"
--- disk_monitor.sh
+++ disk_monitor.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-Threshold=1
-alert="/home/desirae-mertz/Dlmertz_Linux_repository/disk_alert.sh"
+SCRIPT_DIR="${cd "(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCIPT_NAME="$(basname "${BASH_SOURCE[0]}")"
+
+source "${(SCRIPT_DIR}/disk_monitor.conf"
+
+DM_ALERT="${HOME}/disk_alert.sh"
 
 df -h | grep -E '/dev/sd[a-z]|/dev/nvme[0-9]' | while read line
 do
-	filesystem=$(echo $line | awk '{print $1}')
-	percent=$(echo $line | awk '{print $5}')
-	percentNumber=$(echo $percent | tr -d '%')
-	if [ "$percentNumber" -ge "$Threshold" ]
+	DM_FILESYSTEM="$(echo "${line}" | awk '{print "${1}"}')"
+	DM_PERCENT="$(echo "${line}" | awk '{print "${5}"}')"
+	DM_PERCENTNUMBER=$(echo "${DM_PERCENT}" | tr -d '%')
+	if [ "${DM_PERCENTNUMBER}" -ge "${DM_THRESHOLD}" ]
 	then
-		$alert $filesystem $percent
+		"${DM_ALERt}" "${DM_FILESYSTEM}" "${DM_PERCENT}"
 	fi
 done
 
