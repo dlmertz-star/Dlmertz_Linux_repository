fa7ae76420a96f5a9c199f7b8bad112c3a7e2cb6
Desirae Mertz <dlmertz@andrew.cmu.edu>
2026-09-18 13:46:17 -0400

    Attempt to make portable

--- disk_alert.sh
+++ disk_alert.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-Logfile="/home/desirae-mertz/disk-monitor.log"
-filesystem=$1
-percent=$2
-now=$(date --iso-8601=seconds)
-echo "$now $filesystem $percent" >> $Logfile
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+
+source "${SCRIPT_DIR}/disk_monitor.conf"
+
+DA_LOGFILE="~/disk-monitor.log"
+DA_FILESYSTEM=$1
+DA_PERCENT=$2
+DA_NOW=$(date --iso-8601=seconds)
+echo "${now} ${filesystem} ${percent}" >> $Logfile
